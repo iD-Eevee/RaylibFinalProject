132ecc853c0f1904ae0c36cd7d70c3bb1ee6a0db
Eevee <57205370+iD-Eevee@users.noreply.github.com>
2025-05-14 17:19:44 -0400

    Added a comment
    This is my first commit. The project is ready to start.

---
+++ game.cpp
@@ -0,0 +1,32 @@
+
+// DONUT LAND INVASION
+// A Top-Down Action-Adventure Game
+// Created by Eevee
+
+#include "raylib.h"
+
+// This is a test!
+
+// ===================================================================================================================
+int main()
+{
+    // Initialize Window
+    int screenWidth = 800;
+    int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "Top-Down Game");
+
+    SetTargetFPS(60);
+    // ================================================================================================================
+    // Game Loop
+    while (!WindowShouldClose())
+    {
+        //-------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(DARKGREEN);
+
+        EndDrawing();
+        //-------------------------------------------------
+    }
+    CloseWindow();
+}
